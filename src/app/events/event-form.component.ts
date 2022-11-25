0fb4a16b96412e3c15abff15d7619d04fbf42601
RaxMoth <maxfritzroth@gmail.com>
2022-11-25 16:43:47 +0100

    update visuals

--- src/app/events/event-form.component.ts
+++ src/app/events/event-form.component.ts
@@ -7,15 +7,14 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
   styleUrls: ['./event-form.component.css'],
 })
 export class EventFormComponent implements OnInit {
-
   event: Event = new Event('', '', '', '', '', 0, new Date());
-  @Input () editMode!: boolean  
-  @Input() editableEvent!: Event
+  @Input() editMode!: boolean;
+  @Input() editableEvent!: Event;
 
   constructor() {}
 
   ngOnInit(): void {
-  if (this.editMode == true) {
+    if (this.editMode == true) {
       this.event = this.editableEvent;
     }
   }
@@ -23,13 +22,12 @@ export class EventFormComponent implements OnInit {
     this.createEvent.emit(this.event);
 
     //this.event = new Event('', '', '', '', '', 0, new Date());
-
   }
   update(editableEvent: Event): void {
     this.event.id = editableEvent.id;
     this.createEvent.emit(this.event);
   }
-  
+
   @Output()
   createEvent = new EventEmitter();
 }
