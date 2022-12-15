f4df9cf1010419844cbf903832a5e21c013e43f0
RaxMoth <maxfritzroth@gmail.com>
2022-12-15 15:52:40 +0100

    change stuff

--- src/app/models/event.model.spec.ts
+++ src/app/models/event.model.spec.ts
@@ -2,6 +2,8 @@ import { Event } from './event.model';
 
 describe('Event', () => {
   it('should create an instance', () => {
-    expect(new Event('', '', '', '', '', 0, new Date(), false)).toBeTruthy();
+    expect(
+      new Event('', '', '', '', '', 0, new Date(), false, '')
+    ).toBeTruthy();
   });
 });
